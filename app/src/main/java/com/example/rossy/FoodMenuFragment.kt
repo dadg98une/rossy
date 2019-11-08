18bb67103636644a2617adcbdcec37a88c869c58
david <daviddmz0609@gmail.com>
2019-11-07 22:30:02 -0600

    se hacen los fragments

--- app/src/main/java/com/example/rossy/FoodMenuFragment.kt
+++ app/src/main/java/com/example/rossy/FoodMenuFragment.kt
@@ -6,13 +6,13 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 
-class ServicesFragment : Fragment() {
+class FoodMenuFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_services, container, false)
+        return inflater.inflate(R.layout.fragment_foodmenu, container, false)
     }
 }
